9a20980f05f4e89ddee5426069ef5c33e53a0b47
Jinbuhm Kim <jinbuhm.kim@gmail.com>
2019-05-22 14:44:32 +0900

    Update TFT_NTP_Clock_Digital.ino

--- TFT_NTP_Clock_Digital.ino
+++ TFT_NTP_Clock_Digital.ino
@@ -6,17 +6,9 @@
  For a more accurate clock, it would be better to use the RTClib library.
  But this is just a demo...
 
- Make sure all the display driver and pin comnenctions are correct by
- editting the User_Setup.h file in the TFT_eSPI library folder.
-
- #########################################################################
- ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
- #########################################################################
-
  Based on clock sketch by Gilchrist 6/2/2014 1.0
 
 A few colour codes:
-
 code	color
 0x0000	Black
 0xFFFF	White
@@ -30,12 +22,17 @@ code	color
 0x7FF	Cyan
 0x1F	Blue
 0xF81F	Pink
-
  */
+
+/* 
+  1. Based on the code TFT_CLOCK_Digital of M5Stack example.
+  2. Add NTP (Network Time Protocol)
+  3. Need to install NTPClient library https://github.com/taranais/NTPClient
+*/
 
 #include <M5Stack.h>
 #include <WiFi.h>
-#include <NTPClient.h> // https://github.com/taranais/NTPClient
+#include <NTPClient.h> 
 #include <WiFiUdp.h>
 
 #define TFT_GREY 0x5AEB
@@ -55,8 +52,8 @@ String formattedDate;
 String dayStamp;
 String timeStamp;
 
-uint32_t targetTime = 0;                    // for next 1 second timeout
 static uint8_t conv2d(const char* p); // Forward declaration needed for IDE 1.6.x
+uint8_t hh, mm, ss;
 // uint8_t hh = conv2d(__TIME__), mm = conv2d(__TIME__ + 3), ss = conv2d(__TIME__ + 6); // Get H, M, S from compile time
 
 byte omm = 99, oss = 99;
@@ -71,8 +68,6 @@ void setup(void) {
 
   M5.Lcd.setTextSize(1);
   M5.Lcd.setTextColor(TFT_YELLOW, TFT_BLACK);
-
-  targetTime = millis() + 1000;
 
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -94,7 +89,7 @@ void setup(void) {
   // GMT +1 = 3600
   // GMT +8 = 28800
   // GMT -1 = -3600
-  timeClient.setTimeOffset(3600*9);  
+  timeClient.setTimeOffset(3600*9);  // GMT+9
 }
 
 void loop() {
@@ -114,83 +109,57 @@ void loop() {
   dayStamp = formattedDate.substring(0, splitT);
   Serial.print("DATE: ");
   Serial.println(dayStamp);
+
+  M5.Lcd.drawString(dayStamp.c_str(), 0, 0, 1);
+
   // Extract time
   timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
   Serial.print("HOUR: ");
   Serial.println(timeStamp);
 
+  // convert string 
   const char * c = timeStamp.c_str();
 
   hh = conv2d(c);
   mm = conv2d(c+3);
-  ss = conv2d(c+6);
+  ss = conv2d(c+6);  
 
-  Serial.println(hh);
-  Serial.println(mm);
-  Serial.println(ss);
-  
-#if 1
-  // if (targetTime < millis()) {
-    #if 0
-    // Set next update for 1 second later
-    targetTime = millis() + 1000;
+  // Update digital time
+  int xpos = 0;
+  int ypos = 85; // Top left corner ot clock text, about half way down
+  int ysecs = ypos + 24;
 
-    // Adjust the time values by adding 1 second
-    ss++;              // Advance second
-    if (ss == 60) {    // Check for roll-over
-      ss = 0;          // Reset seconds to zero
-      omm = mm;        // Save last minute time for display update
-      mm++;            // Advance minute
-      if (mm > 59) {   // Check for roll-over
-        mm = 0;
-        hh++;          // Advance hour
-        if (hh > 23) { // Check for 24hr roll-over (could roll-over on 13)
-          hh = 0;      // 0 for 24 hour clock, set to 1 for 12 hour clock
-        }
-      }
+  if (omm != mm) { // Redraw hours and minutes time every minute
+    omm = mm;
+    // Draw hours and minutes
+    if (hh < 10) xpos += M5.Lcd.drawChar('0', xpos, ypos, 8); // Add hours leading zero for 24 hr clock
+    xpos += M5.Lcd.drawNumber(hh, xpos, ypos, 8);             // Draw hours
+    xcolon = xpos; // Save colon coord for later to flash on/off later
+    xpos += M5.Lcd.drawChar(':', xpos, ypos - 8, 8);
+    if (mm < 10) xpos += M5.Lcd.drawChar('0', xpos, ypos, 8); // Add minutes leading zero
+    xpos += M5.Lcd.drawNumber(mm, xpos, ypos, 8);             // Draw minutes
+    xsecs = xpos; // Sae seconds 'x' position for later display updates
+  }
+  if (oss != ss) { // Redraw seconds time every second
+    oss = ss;
+    xpos = xsecs;
+
+    if (ss % 2) { // Flash the colons on/off
+      M5.Lcd.setTextColor(0x39C4, TFT_BLACK);        // Set colour to grey to dim colon
+      M5.Lcd.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
+      xpos += M5.Lcd.drawChar(':', xsecs, ysecs, 6); // Seconds colon
+      M5.Lcd.setTextColor(TFT_YELLOW, TFT_BLACK);    // Set colour back to yellow
     }
-    #endif
-
-
-    // Update digital time
-    int xpos = 0;
-    int ypos = 85; // Top left corner ot clock text, about half way down
-    int ysecs = ypos + 24;
-
-    if (omm != mm) { // Redraw hours and minutes time every minute
-      omm = mm;
-      // Draw hours and minutes
-      if (hh < 10) xpos += M5.Lcd.drawChar('0', xpos, ypos, 8); // Add hours leading zero for 24 hr clock
-      xpos += M5.Lcd.drawNumber(hh, xpos, ypos, 8);             // Draw hours
-      xcolon = xpos; // Save colon coord for later to flash on/off later
-      xpos += M5.Lcd.drawChar(':', xpos, ypos - 8, 8);
-      if (mm < 10) xpos += M5.Lcd.drawChar('0', xpos, ypos, 8); // Add minutes leading zero
-      xpos += M5.Lcd.drawNumber(mm, xpos, ypos, 8);             // Draw minutes
-      xsecs = xpos; // Sae seconds 'x' position for later display updates
+    else {
+      M5.Lcd.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
+      xpos += M5.Lcd.drawChar(':', xsecs, ysecs, 6); // Seconds colon
     }
-    if (oss != ss) { // Redraw seconds time every second
-      oss = ss;
-      xpos = xsecs;
 
-      if (ss % 2) { // Flash the colons on/off
-        M5.Lcd.setTextColor(0x39C4, TFT_BLACK);        // Set colour to grey to dim colon
-        M5.Lcd.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
-        xpos += M5.Lcd.drawChar(':', xsecs, ysecs, 6); // Seconds colon
-        M5.Lcd.setTextColor(TFT_YELLOW, TFT_BLACK);    // Set colour back to yellow
-      }
-      else {
-        M5.Lcd.drawChar(':', xcolon, ypos - 8, 8);     // Hour:minute colon
-        xpos += M5.Lcd.drawChar(':', xsecs, ysecs, 6); // Seconds colon
-      }
-
-      //Draw seconds
-      if (ss < 10) xpos += M5.Lcd.drawChar('0', xpos, ysecs, 6); // Add leading zero
-      M5.Lcd.drawNumber(ss, xpos, ysecs, 6);                     // Draw seconds
-    }
-  // }
-#endif  
+    //Draw seconds
+    if (ss < 10) xpos += M5.Lcd.drawChar('0', xpos, ysecs, 6); // Add leading zero
+    M5.Lcd.drawNumber(ss, xpos, ysecs, 6);                     // Draw seconds
+  }
 }
-
 
 // Function to extract numbers from compile time string
 static uint8_t conv2d(const char* p) {
